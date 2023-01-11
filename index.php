8b2c8174d9f982372349ad7469e31a77b93a7114
Wahab Ali Khan <wakhan3@bradford.ac.uk>
2023-01-11 14:02:36 +0000

    starter files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/img/logo.png" type="image/png">
+    <title>Insurance Brokerage and Management System</title>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+</head>
+<body>
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
